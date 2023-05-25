7d35bae44a46eb40b4046a865329f11b7f5eea70
PELENIUM <i.d.chernyshov@yandex.ru>
2023-05-25 23:13:05 +0300

    .

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -34,6 +34,10 @@ namespace Auction
             {
                 try
                 {
+                    /// <TODO>
+                    ///     добавить сортировку по убыванию
+                    /// </TODO>
+
                     float check = float.Parse(LotRate.Text.Replace('.', ','));
 
                     Lot lot = new Lot(ref stackPanel);
@@ -96,37 +100,7 @@ namespace Auction
                     LotInformation.Text = "";
                     LotRate.Text = "";
                 }
-                catch (FormatException)
-                {
-                    /*Lot lot = new Lot(ref stackPanel);
-
-                    StackPanel panel = new StackPanel();
-                    panel.Orientation = Orientation.Horizontal;
-
-                    lot.InformationLabel.Content = LotInformation.Text;
-                    lot.RateLabel.Content = "";
-
-                    Lots.Add(lot);
-
-                    Button DeleteButton = new Button();
-                    DeleteButton.Width = 25;
-                    DeleteButton.Height = 25;
-                    DeleteButton.Content = "X";
-                    DeleteButton.Foreground = Brushes.White;
-                    DeleteButton.Background = new SolidColorBrush(Color.FromArgb(255, 46, 56, 58));
-                    DeleteButton.Margin = new Thickness(0, 10, 0, 0);
-
-                    DeleteButton.Click += (s, ev) => DeleteButton_Click(sender, e, panel);
-
-                    panel.Children.Add(DeleteButton);
-                    panel.Children.Add(lot.InformationLabel);
-                    panel.Children.Add(lot.RateLabel);
-
-                    stackPanel.Children.Add(panel);
-
-                    LotInformation.Text = "";
-                    LotRate.Text = "";*/
-                }
+                catch (FormatException) { }
             }
         }
 
