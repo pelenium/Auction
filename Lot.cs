dc0e45699891551bcfbb91d0758df751087797b3
pelenium <i.d.chernyshov@yandex.ru>
2023-05-25 18:13:01 +0300

    First files
    .

---
+++ Lot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace Auction
+{
+    internal class Lot
+    {
+        public Label InformationLabel = new Label();
+        public Label RateLabel = new Label();
+
+        public Lot()
+        {
+            InformationLabel.FontSize = 20;
+            RateLabel.FontSize = 20;
+
+            InformationLabel.Foreground = Brushes.White;
+            RateLabel.Foreground = Brushes.White;
+
+            InformationLabel.Background = Brushes.Red;
+            RateLabel.Background = Brushes.Blue;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Auction
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Lot> Lots = new List<Lot>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (LotInformation.Text != null && LotRate != null)
+            {
+                Lot lot = new Lot();
+
+                lot.InformationLabel.Content = LotInformation.Text;
+                lot.RateLabel.Content = LotRate.Text;
+
+                Lots.Add(lot);
+
+                lot.InformationLabel.Width = MainForm.ActualWidth * 0.62 * 0.65;
+                lot.InformationLabel.Margin = new Thickness(MainForm.ColumnDefinitions[1].Width, 10, 0, 0);
+                lot.RateLabel.Margin = new Thickness(lot.InformationLabel.Width, -32, 0, 0);
+
+                stackPanel.Children.Add(lot.InformationLabel);
+                stackPanel.Children.Add(lot.RateLabel);
+                LotPanel.UpdateLayout();
+
+                Canvas.SetLeft(lot.InformationLabel, 0);
+                Canvas.SetTop(lot.InformationLabel, 38 * (Lots.Count() + 1) - 30);
+
+                
+            }
+        }
+    }
+}
