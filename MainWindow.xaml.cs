06b8d78dcfc3fa3ff1511b5193e8af65bb2b3eca
PELENIUM <i.d.chernyshov@yandex.ru>
2023-05-28 12:53:30 +0300

    .
    .

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Auction
 {
@@ -21,16 +14,97 @@ namespace Auction
     public partial class MainWindow : Window
     {
         List<Lot> Lots = new List<Lot>();
+        private DispatcherTimer timer;
+        private TimeSpan time;
 
-        /// <TODO>
-        ///     добавить сортировку по убыванию
-        /// </TODO>
-        
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Инициализация таймера
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+
+            // Изначальное время
+
+            time = new TimeSpan(0, 0, 0);
         }
 
+        #region Timer_Block
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            time = time.Subtract(TimeSpan.FromSeconds(1));
+
+            if (time.TotalSeconds <= 0)
+            {
+                timer.Stop();
+                MessageBox.Show("Время истекло!");
+                time= TimeSpan.Zero;
+            }
+
+            UpdateTimerDisplay();
+        }
+
+        private void UpdateTimerDisplay()
+        {
+            // Отображение времени в формате "hh:mm:ss"
+            timerLabel.Content = time.ToString(@"hh\:mm\:ss");
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (time.TotalSeconds > 0)
+            {
+                timer.Start();
+            }
+        }
+
+        private void pauseButton_Click(Object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void resetButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+            time = new TimeSpan(0, 0, 0);
+            UpdateTimerDisplay();
+        }
+
+        private void addMinuteButton_Click(object sender, RoutedEventArgs e)
+        {
+            time = time.Add(TimeSpan.FromMinutes(1));
+            UpdateTimerDisplay();
+        }
+
+        private void subtractMinuteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (time.TotalMinutes >= 1)
+            {
+                time = time.Subtract(TimeSpan.FromMinutes(1));
+                UpdateTimerDisplay();
+            }
+        }
+
+        private void setTimerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TimeSpan.TryParse(timerTextBox.Text, out TimeSpan userInputTime))
+            {
+                time = userInputTime;
+                UpdateTimerDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Некорректный формат времени. Пожалуйста, введите время в формате hh:mm:ss");
+            }
+        }
+
+        #endregion
+
+        #region LotAdding
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
             if (LotInformation.Text != "" && LotRate.Text != "")
@@ -61,7 +135,7 @@ namespace Auction
                     additionToRate.BorderThickness = new Thickness(1);
                     additionToRate.BorderBrush = Brushes.White;
                     additionToRate.Margin = new Thickness(5, 10, 0, 0);
-                    
+
                     additionToRate.LostFocus += (s, ev) => RateAdd_EndChange(s, ev, lot.RateLabel, lot.RateLabel.Content.ToString(), additionToRate);
 
                     Label plusSign = new Label();
@@ -117,6 +191,8 @@ namespace Auction
             {
                 label.Content = rate;
             }
-}
+        }
+
+        #endregion
     }
 }
