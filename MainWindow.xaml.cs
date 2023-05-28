63745d0230ba7d11bd5cfb877bf6ab4279b508c5
PELENIUM <i.d.chernyshov@yandex.ru>
2023-05-28 12:46:17 +0300

    Adding Timer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -114,30 +114,90 @@ namespace Auction
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            if (LotInformation.Text != null && LotRate != null)
+            if (LotInformation.Text != "" && LotRate.Text != "")
             {
-                Lot lot = new Lot();
+                try
+                {
 
-                lot.InformationLabel.Content = LotInformation.Text;
-                lot.RateLabel.Content = LotRate.Text;
+                    float check = float.Parse(LotRate.Text.Replace('.', ','));
 
-                Lots.Add(lot);
+                    Lot lot = new Lot(ref stackPanel);
 
-                lot.InformationLabel.HorizontalAlignment = HorizontalAlignment.Left;
-                lot.InformationLabel.Margin = new Thickness(0, 10, 0, 0);
-                lot.RateLabel.HorizontalAlignment = HorizontalAlignment.Left;
+                    StackPanel panel = new StackPanel();
+                    panel.Orientation = Orientation.Horizontal;
 
-                stackPanel.Children.Add(lot.InformationLabel);
-                stackPanel.Children.Add(lot.RateLabel);
+                    lot.InformationLabel.Content = LotInformation.Text;
+                    lot.RateLabel.Content = LotRate.Text.Replace(',', '.');
 
+                    Lots.Add(lot);
 
-                Canvas.SetLeft(lot.InformationLabel, 0);
-                Canvas.SetTop(lot.InformationLabel, 38 * (Lots.Count() + 1) - 30);
+                    TextBox additionToRate = new TextBox();
+                    additionToRate.Width = 150;
+                    additionToRate.Height = 38;
+                    additionToRate.FontSize = 20;
+                    additionToRate.Foreground = Brushes.White;
+                    additionToRate.Background = new SolidColorBrush(Color.FromArgb(255, 36, 36, 36));
+                    additionToRate.HorizontalContentAlignment = HorizontalAlignment.Center;
+                    additionToRate.VerticalContentAlignment = VerticalAlignment.Center;
+                    additionToRate.BorderThickness = new Thickness(1);
+                    additionToRate.BorderBrush = Brushes.White;
+                    additionToRate.Margin = new Thickness(5, 10, 0, 0);
 
-                
+                    additionToRate.LostFocus += (s, ev) => RateAdd_EndChange(s, ev, lot.RateLabel, lot.RateLabel.Content.ToString(), additionToRate);
+
+                    Label plusSign = new Label();
+                    plusSign.FontSize = 20;
+                    plusSign.Width = 38;
+                    plusSign.Height = 38;
+                    plusSign.Foreground = Brushes.White;
+                    plusSign.Background = new SolidColorBrush(Color.FromArgb(255, 36, 36, 36));
+                    plusSign.BorderThickness = new Thickness(1);
+                    plusSign.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+                    plusSign.VerticalContentAlignment = VerticalAlignment.Bottom;
+                    plusSign.HorizontalContentAlignment = HorizontalAlignment.Center;
+                    plusSign.Margin = new Thickness(5, 10, 0, 0);
+                    plusSign.Content = "➕";
+
+                    Button DeleteButton = new Button();
+                    DeleteButton.Width = 38;
+                    DeleteButton.Height = 38;
+                    DeleteButton.Content = "🗑️";
+                    DeleteButton.BorderThickness = new Thickness(1);
+                    DeleteButton.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255));
+                    DeleteButton.Foreground = Brushes.White;
+                    DeleteButton.Background = new SolidColorBrush(Color.FromArgb(255, 46, 56, 58));
+                    DeleteButton.Margin = new Thickness(0, 10, 0, 0);
+
+                    DeleteButton.Click += (s, ev) => DeleteButton_Click(sender, e, panel);
+
+                    panel.Children.Add(DeleteButton);
+                    panel.Children.Add(lot.InformationLabel);
+                    panel.Children.Add(lot.RateLabel);
+                    panel.Children.Add(plusSign);
+                    panel.Children.Add(additionToRate);
+
+                    stackPanel.Children.Add(panel);
+
+                    LotInformation.Text = "";
+                    LotRate.Text = "";
+                }
+                catch (FormatException) { }
             }
         }
 
+        private void DeleteButton_Click(object sender, RoutedEventArgs e, StackPanel panel) => stackPanel.Children.Remove(panel);
 
+        private void RateAdd_EndChange(object sender, RoutedEventArgs e, Label label, string rate, TextBox adding)
+        {
+            try
+            {
+                label.Content = (float.Parse(rate.Replace('.', ',')) + float.Parse(adding.Text.Replace('.', ','))).ToString().Replace(',', '.');
+                adding.Text = "";
+            }
+            catch (FormatException)
+            {
+                label.Content = rate;
+            }
+        }
     }
 }
